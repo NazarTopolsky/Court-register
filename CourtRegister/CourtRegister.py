0eac6ecd82eb4e1502fe008361de478f1331613e
n <nazartopolsky@gmail.com>
2015-06-02 02:05:02 +0300

    Kinda done

--- CourtRegister/CourtRegister.py
+++ CourtRegister/CourtRegister.py
@@ -8,11 +8,17 @@ app = Flask(__name__, static_url_path='/static/')
 gdb = GraphDatabase("http://localhost:7474")
 
 
-@app.route('/test')
+@app.route('/', methods=['GET', 'POST'])
 def test():
-    nodes, rels = generate_graph(limit=25)
-    js = dumps({"nodes": nodes, "edges": rels})
+    if request.method == 'GET':
+        nodes, rels = generate_graph(limit=25)
+        js = dumps({"nodes": nodes, "edges": rels})
+    else:
+        neo4j_query = query_builder(request.form)
+        nodes, rels = generate_graph(25, neo4j_query)
+        js = dumps({"nodes": nodes, "edges": rels})
     return render_template("test.html", json=js)
+
 
 
 def __timestamp__(_datetime):
@@ -21,19 +27,6 @@ def __timestamp__(_datetime):
         return datetime(d[2], d[1], d[0], 0, 0, 0).timestamp()
     else:
         return _datetime
-
-
-@app.route('/', methods=['GET', 'POST'])
-def index():
-    if request.method == 'GET':
-        nodes, rels = generate_graph(limit=25)
-        js = dumps({"nodes": nodes, "edges": rels})
-        return render_template('index.html', json=js)
-    else:
-        neo4j_query = query_builder(request.form)
-        nodes, rels = generate_graph(25, neo4j_query)
-        js = dumps({"nodes": nodes, "edges": rels})
-        return render_template('index.html', json=js)
 
 
 def where_builder(params):
@@ -163,19 +156,23 @@ def generate_graph(limit=25, query=None):
         caption = rel['metadata']['type']
         return start, end, caption
 
-    def make_data(_id, _dict):
+    def make_data(_dict):
         allowed = {
             'case_number': 'Номер справи',
             'link': 'Посилання',
-            'reg_date': 'Дата:',
+            'reg_date': 'Дата',
             'law_date': 'Дата набуття законної сили',
             'name': 'name'
         }
         print(_dict)
-        data = {'id': _id}
+        data = {}
         for key in _dict:
             if _dict[key] is not None and _dict[key] != '' and key in allowed:
-                data[allowed[key]] = _dict[key]
+                if 'date' in key:
+                    val = datetime.fromtimestamp(_dict[key])
+                    data[allowed[key]] = str(val).split(' ')[0]
+                else:
+                    data[allowed[key]] = _dict[key]
         return data
 
     query = query or query_builder(limit=limit)
@@ -185,19 +182,15 @@ def generate_graph(limit=25, query=None):
         # nodes
         print(case['data'])
         print(chairman['data'])
-        g.push_node(region['metadata']['id'],   region['data']['name'],   'Region',
-                    data=make_data(region['metadata']['id'], region['data']))
-        g.push_node(court['metadata']['id'],    court['data']['name'],    'Court',
-                    data=make_data(court['metadata']['id'], court['data']))
-        g.push_node(chairman['metadata']['id'], chairman['data']['name'], 'Chairman',
-                    data=make_data(chairman['metadata']['id'], chairman['data']))
-        # merging it with case
+        g.push_node(region['metadata']['id'],   region['data']['name'],   'Region', data=make_data(region['data']))
+        g.push_node(court['metadata']['id'],    court['data']['name'],    'Court', data=make_data(court['data']))
+        g.push_node(chairman['metadata']['id'], chairman['data']['name'], 'Chairman', data=make_data(chairman['data']))
+        # merged with case
         # g.push_node(ct['metadata']['id'],       ct['data']['name'],                'Court Decision Type')
         # g.push_node(vt['metadata']['id'],       vt['data']['name'],                'Court Judgement Type')
-        case_data = make_data(case['metadata']['id'], case['data'])
-        case['Court Decision Type'] = ct['data']['name']
-        case['Court Judgement Type'] = vt['data']['name']
-        #
+        case_data = make_data(case['data'])
+        case['Форма судочинства'] = ct['data']['name']
+        case['Форма судового рішення'] = vt['data']['name']
         g.push_node(case['metadata']['id'], '№' + case['data']['case_number'], 'Court case', data=case_data)
 
         source, target, caption = _link(crt_r)
@@ -208,6 +201,7 @@ def generate_graph(limit=25, query=None):
         # c_ch
         source, target, caption = _link(c_ch)
         g.push_link(source, target, caption)
+        # merged with case
         # c_ct
         # source, target, caption = _link(c_ct)
         # g.push_link(source, target, caption)
@@ -215,75 +209,6 @@ def generate_graph(limit=25, query=None):
         # source, target, caption = _link(c_vt)
         # g.push_link(source, target, caption)
     return g.nodes, g.links
-
-# original:
-# def node(title, label, _id, cluster, ids):
-#     return {'name': title, 'caption': title, 'node_type': label, 'id': ids[_id], 'cluster': cluster}
-#
-#
-# def relation(rel, ids, cluster):
-#     start = int(rel['start'].split('/')[-1])
-#     end = int(rel['end'].split('/')[-1])
-#     _type = rel['metadata']['type']
-#     return {'source': ids[start], 'target': ids[end], 'caption': _type, 'cluster': cluster}
-#
-#
-# def generate_graph(limit=25):
-#     query = query_builder()
-#     results = gdb.query(query,
-#                         params={"limit": limit})
-#     nodes = []
-#     rels = []
-#     ids_dict = {}
-#     i = 0
-#     for region, court, case, chairman, ct, vt, crt_r, c_crt, c_ch, c_ct, c_vt in results:
-#         # region
-#         reg_node = node(region['data']['name'], 'Region', region['metadata']['id'], 1)
-#         if reg_node not in nodes:
-#             nodes.append(reg_node)
-#         # court
-#         court_node = node(court['data']['name'], 'Court', court['metadata']['id'], 2)
-#         if court_node not in nodes:
-#             nodes.append(court_node)
-#         # chairman
-#         chairman_node = node(chairman['data']['name'], 'Chairman', chairman['metadata']['id'], 3)
-#         if chairman_node not in nodes:
-#             nodes.append(chairman_node)
-#         # ct
-#         ct_node = node(ct['data']['name'], 'Court Decision Type', ct['metadata']['id'], 4)
-#         if ct_node not in nodes:
-#             nodes.append(ct_node)
-#         # vt
-#         vt_node = node(vt['data']['name'], 'Court Judgement Type', vt['metadata']['id'], 5)
-#         if vt_node not in nodes:
-#             nodes.append(vt_node)
-#         # case
-#         # TODO: INFO!!!1
-#         case_node = node('№' + case['data']['case_number'], 'Court case', case['metadata']['id'], 6)
-#         if case_node not in nodes:
-#             # print(case['data'])
-#             nodes.append(case_node)
-#         # crt_r
-#         crt_r_rel = relation(crt_r, 7)
-#         if crt_r_rel not in rels:
-#             rels.append(crt_r_rel)
-#         # c_crt
-#         c_crt_rel = relation(c_crt, 8)
-#         if c_crt_rel not in rels:
-#             rels.append(c_crt_rel)
-#         # c_ch
-#         c_ch_rel = relation(c_ch, 9)
-#         if c_ch_rel not in rels:
-#             rels.append(c_ch_rel)
-#         # c_ct
-#         c_ct_rel = relation(c_ct, 10)
-#         if c_ct_rel not in rels:
-#             rels.append(c_ct_rel)
-#         # c_vt
-#         c_vt_rel = relation(c_vt, 11)
-#         if c_vt_rel not in rels:
-#             rels.append(c_vt_rel)
-#     return nodes, rels
 
 
 if __name__ == '__main__':
