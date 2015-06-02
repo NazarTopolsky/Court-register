fa62a56dd3bcd44b74ba301c758be66b2b51d7f6
n <nazartopolsky@gmail.com>
2015-06-02 16:51:13 +0300

    Cleanup

--- CourtRegister/CourtRegister.py
+++ CourtRegister/CourtRegister.py
@@ -1,25 +1,41 @@
 from json import dumps
 from neo4jrestclient.client import GraphDatabase
-from flask import Flask, Response, request, render_template
+from flask import Flask, Response, request, render_template, send_from_directory
 from datetime import datetime
 from re import findall
 
 app = Flask(__name__, static_url_path='/static/')
 gdb = GraphDatabase("http://localhost:7474")
 
-
 @app.route('/', methods=['GET', 'POST'])
-def test():
+def index():
     if request.method == 'GET':
         nodes, rels = generate_graph(limit=25)
         js = dumps({"nodes": nodes, "edges": rels})
     else:
-        neo4j_query = query_builder(request.form)
-        nodes, rels = generate_graph(25, neo4j_query)
+        try:
+            limit = int(request.form['limit'])
+        except:
+            limit=25
+        print(limit)
+        neo4j_query = query_builder(request.form, limit)
+        nodes, rels = generate_graph(limit=limit, query=neo4j_query)
         js = dumps({"nodes": nodes, "edges": rels})
-    return render_template("test.html", json=js)
+    return render_template("index.html", json=js)
 
+@app.route('/js')
+def js():
+    return app.send_static_file('d3.v2.js')
 
+@app.route("/graph")
+def get_graph():
+    nodes, rels = generate_graph(25)
+    s = dumps({"nodes": nodes, "edges": rels})
+    try:
+        return Response(s, mimetype="application/json")
+    except BaseException as e:
+        print(e.__str__())
+        return Response(dumps({'error: ': e.__str__()}))
 
 def __timestamp__(_datetime):
     if _datetime is not None and _datetime != '' and not isinstance(_datetime, float):
@@ -27,7 +43,6 @@ def __timestamp__(_datetime):
         return datetime(d[2], d[1], d[0], 0, 0, 0).timestamp()
     else:
         return _datetime
-
 
 def where_builder(params):
     try:
@@ -72,8 +87,13 @@ def where_builder(params):
         result = ''
     return result
 
-
 def query_builder(where_dict=None, limit=25, order=None):
+    query_base = """
+MATCH (r:Region)<-[crt_r:SITUATED_IN]-(crt:Court)<-[c_crt:RULED_BY]-(c:Case)-[c_ch:CARRIED_BY]->(ch:Chairman),
+(ct:Court_Decision_Type)-[c_ct]-c-[c_vt]-(vt:Court_Judgement_Type)"""
+    query_return = """
+RETURN r, crt, c, ch, ct, vt, crt_r, c_crt, c_ch, c_ct, c_vt
+"""
     final_query = query_base
     if where_dict:
         final_query += where_builder(where_dict)
@@ -82,36 +102,6 @@ def query_builder(where_dict=None, limit=25, order=None):
         final_query += 'ORDER BY ' + order + ' '
     final_query += 'LIMIT ' + str(limit)
     return final_query
-
-
-query_base = """
-MATCH (r:Region)<-[crt_r:SITUATED_IN]-(crt:Court)<-[c_crt:RULED_BY]-(c:Case)-[c_ch:CARRIED_BY]->(ch:Chairman),
-(ct:Court_Decision_Type)-[c_ct]-c-[c_vt]-(vt:Court_Judgement_Type)
-"""
-query_return = """
-RETURN r, crt, c, ch, ct, vt, crt_r, c_crt, c_ch, c_ct, c_vt
-"""
-
-query_sample = """
-match (r:Region)<-[crt_r:SITUATED_IN]-(crt:Court)<-[c_crt:RULED_BY]-(c:Case)-[c_ch:CARRIED_BY]->(ch:Chairman),
-(ct:Court_Decision_Type)-[c_ct]-c-[c_vt]-(vt:Court_Judgement_Type)
-where r.name='Київська область'
-return r, crt, c, ch, ct, vt, c_crt
-order by vt.name
-limit 10
-"""
-
-
-@app.route("/graph")
-def get_graph():
-    nodes, rels = generate_graph(25)
-    s = dumps({"nodes": nodes, "edges": rels})
-    try:
-        return Response(s, mimetype="application/json")
-    except BaseException as e:
-        print(e.__str__())
-        return Response(dumps({'error: ': e.__str__()}))
-
 
 class GraphMaker:
     def __init__(self):
@@ -148,7 +138,6 @@ class GraphMaker:
             self.links.append(_link)
         return _link
 
-
 def generate_graph(limit=25, query=None):
     def _link(rel):
         start = int(rel['start'].split('/')[-1])
@@ -164,7 +153,6 @@ def generate_graph(limit=25, query=None):
             'law_date': 'Дата набуття законної сили',
             'name': 'name'
         }
-        print(_dict)
         data = {}
         for key in _dict:
             if _dict[key] is not None and _dict[key] != '' and key in allowed:
@@ -176,12 +164,11 @@ def generate_graph(limit=25, query=None):
         return data
 
     query = query or query_builder(limit=limit)
+    print(query)
     results = gdb.query(query, params={"limit": limit})
     g = GraphMaker()
     for region, court, case, chairman, ct, vt, crt_r, c_crt, c_ch, c_ct, c_vt in results:
         # nodes
-        print(case['data'])
-        print(chairman['data'])
         g.push_node(region['metadata']['id'],   region['data']['name'],   'Region', data=make_data(region['data']))
         g.push_node(court['metadata']['id'],    court['data']['name'],    'Court', data=make_data(court['data']))
         g.push_node(chairman['metadata']['id'], chairman['data']['name'], 'Chairman', data=make_data(chairman['data']))
@@ -189,8 +176,8 @@ def generate_graph(limit=25, query=None):
         # g.push_node(ct['metadata']['id'],       ct['data']['name'],                'Court Decision Type')
         # g.push_node(vt['metadata']['id'],       vt['data']['name'],                'Court Judgement Type')
         case_data = make_data(case['data'])
-        case['Форма судочинства'] = ct['data']['name']
-        case['Форма судового рішення'] = vt['data']['name']
+        case_data['Форма судочинства'] = ct['data']['name']
+        case_data['Форма судового рішення'] = vt['data']['name']
         g.push_node(case['metadata']['id'], '№' + case['data']['case_number'], 'Court case', data=case_data)
 
         source, target, caption = _link(crt_r)
@@ -209,7 +196,6 @@ def generate_graph(limit=25, query=None):
         # source, target, caption = _link(c_vt)
         # g.push_link(source, target, caption)
     return g.nodes, g.links
-
 
 if __name__ == '__main__':
     app.run()
